4a56679f6c832aa452ab23bbc3f0422f1b16e344
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-28 00:20:44 +0100

    feat: 1-fifo_cache.py

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" a class FIFOCache that inherits
+from BaseCaching and is a caching system
+"""
+
+
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class FIFOCache(BaseCaching):
+    """ a class FIFOCache that inherits
+    from BaseCaching and is a caching system
+    """
+    def __init__(self):
+        """Inherit from parent"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """"put() function"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_item, _last_item = self.cache_data.popitem(False)
+            # Or first_item_deleted = self.cache_data.pop(0)
+            print("DISCARD: {}".format(first_item))
+
+    def get(self, key):
+        """get() function"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
+        # or self.cache_data.get(key, None)
