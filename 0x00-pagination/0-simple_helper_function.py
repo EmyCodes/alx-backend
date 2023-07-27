8a2d238bf791c2dff5381f5bfbcf97b2ccb28d1b
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-27 16:31:47 +0100

    style: code to pass pycodestyle

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -10,10 +10,11 @@ Page numbers are 1-indexed, i.e. the first page is page 1.
 
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """e function should return a tuple of size two 
-    containing a start index and an end 
-    index corresponding to the range of indexes to return in 
+    """function should return a tuple of size two
+    containing a start index and an end
+    index corresponding to the range of indexes to return in
     a list for those particular pagination parameters
     """
     if page < 1:
